9f16136888c0a9ba96aa2d9ae46a49bb98118051
ADHIL23 <139318476+ADHIL23@users.noreply.github.com>
2023-07-14 17:30:54 +0530

    Add files via upload

---
+++ printinteger.java
@@ -0,0 +1,13 @@
+package gtjune2023;
+import java.util.Scanner;
+
+public class printinteger {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.print("enter  a number:");
+		int number = reader.nextInt();
+		System.out.println("you entered:" + number);
+		
+	}
+
+}
