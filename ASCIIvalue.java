c495fcb1fe0645dffe68179eccb49ec5de8002b9
ADHIL23 <139318476+ADHIL23@users.noreply.github.com>
2023-07-14 18:11:37 +0530

    Add files via upload

---
+++ ASCIIvalue.java
@@ -0,0 +1,12 @@
+package gtjune2023;
+
+public class ASCIIvalue {
+	public static void main(String[] args) {
+		char ch = 'H';
+		int ascii = ch;
+		int castAscii = (int) ch;
+		System.out.println("the ASCII value of " + ch + "is: " + ascii);
+		System.out.println("the ASCII value of " + ch + "is: " + castAscii);
+	}
+
+}
