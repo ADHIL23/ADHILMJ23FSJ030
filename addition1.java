df56f35d2fe5380ec599aa14a4ebe84f5205a67f
ADHIL23 <139318476+ADHIL23@users.noreply.github.com>
2023-07-14 17:10:54 +0530

    Add files via upload

---
+++ addition1.java
@@ -0,0 +1,13 @@
+package gtjune2023;
+
+public class addition1 {
+	public static void main(String[] args)
+	{
+		System.out.println("enter two numbers");
+		int first=100;
+		int second=200;
+		System.out.println(first + " " + second);
+		int sum = first + second;
+		System.out.println("the sum is:" + sum);
+	}
+}
