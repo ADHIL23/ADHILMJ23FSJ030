5c1aeda4c25410471fc7188e1a61d7212f46488b
ADHIL23 <139318476+ADHIL23@users.noreply.github.com>
2023-07-14 17:53:27 +0530

    Add files via upload

---
+++ multiplytwonumbers.java
@@ -0,0 +1,17 @@
+package gtjune2023;
+import java.util.Scanner;
+
+
+public class multiplytwonumbers {
+		public static void main(String[] args) {
+			Scanner scan=new Scanner(System.in);
+			System.out.println("enter value for i...:");
+			int i=scan.nextInt();
+			System.out.println("enter value for j...:");
+			int j=scan.nextInt();
+			int mul=i*j;
+			System.out.println("the multiplication of i and j is ...:"+mul);
+			
+		}
+	}
+
