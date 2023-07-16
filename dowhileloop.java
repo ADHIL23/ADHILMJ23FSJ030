df454bed91b61fdff2d69d3676ec4226a12da4f7
ADHIL23 <139318476+ADHIL23@users.noreply.github.com>
2023-07-16 16:32:12 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,14 @@
+package gtjune2023;
+
+public class dowhileloop {
+	public static void main(String args[]) {
+		int x =1;
+		
+		do {
+			System.out.print("value of x : " + x );
+			x++;
+			System.out.print("\n");
+		}while( x < 11);
+	}
+
+}
