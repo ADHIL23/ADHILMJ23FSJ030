4ef0ce3c06c743eed9cbb9dc68c9a9e56cb93653
ADHIL23 <139318476+ADHIL23@users.noreply.github.com>
2023-08-03 13:11:24 +0530

    Add files via upload

---
+++ primitivedatatypes.java
@@ -0,0 +1,25 @@
+package gtjune2023;
+
+public class primitivedatatypes {
+	public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        float f;
+        double d;
+        char c;
+        boolean bool;
+
+        System.out.println("Default values of primitive types in Java:");
+        System.out.println("byte: " );
+        System.out.println("short: " );
+        System.out.println("int: " );
+        System.out.println("long: " );
+        System.out.println("float: " );
+        System.out.println("double: " );
+        System.out.println("char: " );
+        System.out.println("boolean: " );
+    }
+
+}
